c52702e78039f23a1559f203532377fac2a38d9e
vibhor121 <chinda473@gmail.com>
2025-10-16 22:45:09 +0530

    Added all 4 pages

---
+++ src/pages/RefundPolicy.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const RefundPolicy = () => {
+  return (
+    <div className='flex justify-center items-center flex-col'>
+      <br />
+      <h3 className='uppercase text-3xl text-black text-center font-semibold'>REFUND POLICY</h3>
+      <br />
+      <p className='text-sm max-w-4xl'>
+        <div className='font-bold py-6'>Returns</div>
+
+        We maintain industry standard 7 days return policy.
+        <br />
+        To be eligible for a return, your item must be unused and in the sealed original packaging.
+        <br /><br />
+
+        Several types of goods are exempt from being returned. Perishable goods such as food, flowers, newspapers or Hygiene goods cannot be returned.
+        <br /><br />
+
+        To complete your return, we require a receipt or proof of purchase.
+        <br />
+        Please do not send your purchase back to the manufacturer.
+        <br /><br />
+
+        <span className='font-bold pt-6'>Refunds (if applicable)</span><br /><br />
+        Once your return is received and inspected, we will send you an email to notify you that we have received your returned item.
+        <br />
+        If you are approved, we will issue you an exclusive discount coupon of agreed refund amount which will have no validity and can be used on any of your future purchase.
+        <br /><br />
+
+        Refund processing may take upto 5 - 7 business days.
+        <br /><br />
+
+        <span className='font-style: italic'>For International Shipment</span>  - Refunds on International shipment is only possible for the product value. Shipping charges on international shipment will be not borne by us once the shipment has left our facility.
+        <br /><br />
+
+        <span className='font-bold pt-6'>Product Bought in Sale (if applicable)</span><br /><br />
+        Unfortunately, products bought during sale period cannot be refunded/returned/exchanged if the product is not damaged.
+        <br /><br />
+
+        <span className='font-bold pt-6'>Exchanges (if applicable)</span><br /><br />
+        We only replace items if they are defective or damaged. If you need to exchange it for the same item, send us an email at hello@mywishcare.com and send your item to: 28, B. T. Road, Lohia Press, 700002 KOLKATA WB, India.
+        <br /><br />
+
+        <span className='font-bold pt-6'>Shipping</span><br /><br />
+        To return your product, you should mail your product to: 28, B. T. Road, Lohia Press, 700002 KOLKATA WB, India
+        <br /><br />
+
+        Depending on where you live, the time it may take for your exchanged product to reach you, may vary.
+      </p>
+    </div>
+  )
+}
+
+export default RefundPolicy
+
---
+++ src/pages/ShippingPolicy.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const ShippingPolicy = () => {
+  return (
+    <div className='flex justify-center items-center flex-col'>
+      <br />
+      <h3 className='uppercase text-3xl text-black text-center font-semibold'>SHIPPING POLICY</h3>
+      <br />
+      <p className='text-sm max-w-4xl'>
+        We ship any domestic orders within 2 business days and international orders within business days of your order date.
+        <br /><br />
+
+        Below are the timelines for delivery of domestic orders once the orders are shipped :-
+        <br /><br />
+
+        Metro & Tier 1 cities - 2-5 business days
+        <br /><br />
+
+        Tier 2 cities - 3-7  business days
+        <br /><br />
+
+        Tier 3 cities - 4- 10 business days
+        <br /><br />
+
+        Remote/North East / J&K - 7 -20 business days
+        <br /><br />
+
+        International Shipment - All Shipments will be dispatched within 7 days. All local taxes and import duties for the the consignee will be the responsibility of the customer.
+      </p>
+    </div>
+  )
+}
+
+export default ShippingPolicy
+
